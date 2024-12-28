7ad0b0e70388cf5133baf70b6377cc0adc547204
Your Name <golamsarwar1896@gmail.com>
2024-12-29 02:19:32 +0600

    Added Category Slide Images and Made a reusable component to implement headings and sub headings

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "./Banner";
+import CategorySlider from "./CategorySlider";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <CategorySlider></CategorySlider>
     </div>
   );
 };
